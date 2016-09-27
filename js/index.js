3e31d9f2dbf63cfc34622115f8b4bc16caa774ae
chi5hao <779213209@qq.com>
2016-09-27 18:30:56 +0800

    基本代码~

---
+++ js/index.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+	var screen = document.getElementById('screen');
+
+	window.mySwipe = new Swipe(screen, {
+		startSlide: 0,
+		speed: 400,
+		auto: 3000,
+		continuous: true,
+		disableScroll: false,
+		stopPropagation: false,
+		callback: function(index, elem) {},
+		transitionEnd: function(index, elem) {}
+	});
+
+	var slider = document.getElementById('slider');
+	window.mySwipe = new Swipe(slider, {
+		startSlide: 0,
+		speed: 400,
+		auto: 3000,
+		continuous: true,
+		disableScroll: false,
+		stopPropagation: false,
+		callback: function(index, elem) {
+			var dotlist = document.getElementsByClassName('dot');
+			for(i = 0; i < dotlist.length; i++) {
+				var dot = dotlist[i];
+				dot.className="dot";
+				if(i == index) {
+					dot.className="dot active";
+				}
+			}
+		},
+		transitionEnd: function(index, elem) {}
+	})
+}
